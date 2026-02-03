b6123e1fcead0dc83de7f7ae7b253b8e745b147d
André Luiz da Silva <andreluizdasilva@MacBook-Air-de-Andre.local>
2026-02-03 20:22:16 -0300

    v0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Teste Aplicação");
+
+        Person p = new Person();
+        p.setId(1);
+        p.setName("André");
+        p.setTelephone("(16) 9 99998888");
+
+        Employee e = new Employee();
+        e.setId(2);
+        e.setName("Luiz");
+        e.setTelephone("(16) 9 77776666");
+
+        System.out.println(p);
+        System.out.println(e);
+    }
+}
